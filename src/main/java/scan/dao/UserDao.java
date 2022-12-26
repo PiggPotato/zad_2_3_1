8019abb23e9b45d3cfdf7ae56bc3e1926cab3a45
PiggPotato <aldenmen12@gmail.com>
2022-12-26 17:08:07 +0300

    fianl
    final

---
+++ src/main/java/scan/dao/UserDao.java
@@ -0,0 +1,21 @@
+package scan.dao;
+
+import scan.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void create(User user);
+
+    User show(long id);
+
+    List<User> getList();
+
+    void update(long id, User user);
+
+    void delete(long id);
+
+    void delete(User user);
+
+    List<User> find(User user);
+}
